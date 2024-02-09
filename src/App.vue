daf881ccd0433828e8f70b9165642833991ba254
matteopoliti <148256058+matteopoliti@users.noreply.github.com>
2024-02-09 14:37:39 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+            store,
+        };
+    },
+    created() {
+    },
+}
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
